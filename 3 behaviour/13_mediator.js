3a81167730273e77f936ab5b569fe616a9995b29
Ошуркевич Роман Александрович <Oshurkevich.R.A@sberbank.ru>
2020-11-16 22:01:38 +0400

    js patterns

---
+++ 3 behaviour/13_mediator.js
@@ -0,0 +1,51 @@
+class User {
+    constructor(name) {
+        this.name = name
+        this.room = null
+    }
+
+    send(message, to) {
+        this.room.send(message, this, to)
+    }
+
+    receive(message, from) {
+        console.log(`${from.name} => ${this.name}: ${message}`)
+    }
+}
+
+class ChatRoom {
+    constructor() {
+        this.users = {}
+    }
+
+    register(user) {
+        this.users[user.name] = user
+        user.room = this
+    }
+
+    send(message, from, to) {
+        if (to) {
+            to.receive(message, from)
+        } else {
+            Object.keys(this.users).forEach(key => {
+                if (this.users[key] !== from) {
+                    this.users[key].receive(message, from)
+                }
+            })
+        }
+    }
+}
+
+const roman = new User('Roman')
+const lena = new User('Elena')
+const igor = new User('Igor')
+
+const room = new ChatRoom()
+
+room.register(roman)
+room.register(lena)
+room.register(igor)
+
+roman.send("Hello", lena)
+lena.send("Hello hello!", roman)
+igor.send("Vsem privet")
